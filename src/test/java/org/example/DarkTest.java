4652f68c610a3701d47c1c3557e6101fbc2e7c72
LaenorSama <laenorsama@yandex.ru>
2025-07-29 23:00:44 +0500

    add workflow

--- src/test/java/org/example/DarkTest.java
+++ src/test/java/org/example/DarkTest.java
@@ -6,19 +6,4 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static io.qameta.allure.Allure.step;
 
-public class DarkTest {
 
-    @Test
-    @AllureId("661")
-    @DisplayName("Проверка темной темы.")
-    public void testMethod() {
-        Allure.label("Jira_Alex", "SCRUM-28");
-        Allure.label("suite", "test_calc_classes");
-        step("Зайти на портал");
-        step("Перейти в меню пользователя");
-        step("Открыть настройки оформления");
-        step("Переключить тему оформления на темную");
-        step("Убедиться что интерфейс переключился на темную тему");
-    }
-
-}
